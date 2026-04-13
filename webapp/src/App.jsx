e9eafb9c0b84cd3782a1f668fb98285198897866
Dennis Batista <dennismaxbatista@gmail.com>
2026-04-13 13:16:44 -0400

    reparir all

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -3,15 +3,17 @@ import Scene from "./components/Scene";
 import Navbar from "./components/Navbar";
 import Ruleta from "./components/Ruleta";
 import Mercado from "./components/Mercado";
-import "./styles.css"; // estilos globales
+import "./styles.css";
 
 export default function App() {
-  const [currentTab, setCurrentTab] = useState("ruleta");
+  const [currentTab, setCurrentTab] = useState("scene");
 
   return (
     <div className="app">
-      {currentTab === "ruleta" ? <Ruleta /> : <Scene />}
+      {currentTab === "scene" && <Scene />}
+      {currentTab === "ruleta" && <Ruleta />}
       {currentTab === "mercado" && <Mercado />}
+
       <Navbar currentTab={currentTab} setCurrentTab={setCurrentTab} />
     </div>
   );
